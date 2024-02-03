f3dddcc7687d54ef2ce3a63b303acedf9153e643
Thomas Durand <thomas.durand.verjat@gmail.com>
2024-02-03 12:06:02 +0100

    Using real concurrency checking ^_^

--- Package.swift
+++ Package.swift
@@ -20,5 +20,5 @@ let package = Package(
 )
 
 extension SwiftSetting {
-    static let strictConcurrency = enableUpcomingFeature("StrictConcurrency")
+    static let strictConcurrency = enableExperimentalFeature("StrictConcurrency")
 }
