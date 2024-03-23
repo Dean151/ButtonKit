2106a5ed61455d9ebce7a02bafaa42f8deb22174
Thomas Durand <thomas.durand.verjat@gmail.com>
2024-03-24 00:45:33 +0100

    Added a demo project to demonstrate library capabilities

---
+++ Demo/ButtonKitDemo/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  ButtonKit
+//
+//  MIT License
+//
+//  Copyright (c) 2024 Thomas Durand
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    NavigationLink {
+                        ThrowableButtonDemo()
+                    } label: {
+                        Text("Throwable Button")
+                    }
+
+                    NavigationLink {
+                        AsyncButtonDemo()
+                    } label: {
+                        Text("Async Button")
+                    }
+                } header: {
+                    Text("Basics")
+                }
+
+                Section {
+                    NavigationLink {
+                        DiscreteProgressDemo()
+                    } label: {
+                        Text("Discrete Progress")
+                    }
+
+                    NavigationLink {
+                        EstimatedProgressDemo()
+                    } label: {
+                        Text("Estimated Progress")
+                    }
+                } header: {
+                    Text("Determinate progress")
+                }
+
+                Section {
+                    NavigationLink {
+                        EmptyView()
+                    } label: {
+                        Text("App Store Download")
+                    }
+                } header: {
+                    Text("Customization")
+                }
+            }
+            .navigationTitle("ButtonKit")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
