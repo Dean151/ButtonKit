5955adbc6281e4372eaa4031349bcf967f13da5d
Thomas Durand <thomas.durand.verjat@gmail.com>
2026-01-27 00:20:32 +0100

    chore(license): updated copyright year

--- Sources/ButtonKit/Progress/Progress+Estimated.swift
+++ Sources/ButtonKit/Progress/Progress+Estimated.swift
@@ -4,7 +4,7 @@
 //
 //  MIT License
 //
-//  Copyright (c) 2025 Thomas Durand
+//  Copyright (c) 2026 Thomas Durand
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
